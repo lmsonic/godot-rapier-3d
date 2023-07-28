43de39cb8776c3800515d36b08c6b406338b3e43
Iride Massidda <lorenzomassidda@hotmail.com>
2023-07-28 17:56:44 +0200

    registering classes

--- rapier-godot/src/lib.rs
+++ rapier-godot/src/lib.rs
@@ -4,7 +4,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use godot::{engine::PhysicsServer3DManager, prelude::*};
+use godot::{
+    engine::PhysicsServer3DManager, prelude::*, private::class_macros::auto_register_classes,
+};
 use physics_server_3d::RapierPhysicsServer3D;
 
 struct RapierPhysics;
@@ -47,9 +49,23 @@ impl ExtensionLayer for ServerLayer {
     fn deinitialize(&mut self) {}
 }
 
+struct DefaultLayer;
+
+impl ExtensionLayer for DefaultLayer {
+    fn initialize(&mut self) {
+        crate::auto_register_classes();
+    }
+
+    fn deinitialize(&mut self) {
+        // Nothing -- note that any cleanup task should be performed outside of this method,
+        // as the user is free to use a different impl, so cleanup code may not be run.
+    }
+}
+
 #[gdextension]
 unsafe impl ExtensionLibrary for RapierPhysics {
     fn load_library(handle: &mut InitHandle) -> bool {
+        handle.register_layer(InitLevel::Scene, DefaultLayer);
         handle.register_layer(InitLevel::Servers, ServerLayer::new());
         true
     }
